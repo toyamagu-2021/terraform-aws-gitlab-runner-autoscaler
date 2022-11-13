840ec7e9b1ed7d9c837876d336b82c79cc902ae6
toyamagu2021@gmail.com <toyamagu2021@gmail.com>
2022-11-13 16:55:31 +0900

    remove user_data from outputs
    Signed-off-by: toyamagu2021@gmail.com <toyamagu2021@gmail.com>

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -12,8 +12,3 @@ output "gitlab_public_dns" {
   description = "GitLab instance public dns"
   value       = module.gitlab.public_dns
 }
-
-output "gitlab_user_data" {
-  description = "GitLab instance user data"
-  value       = local.gitlab_user_data
-}
